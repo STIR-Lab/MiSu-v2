4fc6de6322338447fa435998cd1f7b7ad0e8cda7
pdcolandrea <colandreapaul@knights.ucf.edu>
2021-06-09 22:41:37 -0400

    Add guest modals

--- components/GuestElement.js
+++ components/GuestElement.js
@@ -1,31 +1,35 @@
 import React, { useState } from 'react';
-import { Image, StyleSheet, TouchableOpacity, View, Text, TouchableWithoutFeedback } from 'react-native';
-
+import {
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text,
+  TouchableWithoutFeedback,
+} from 'react-native';
 
 function GuestElement(props) {
-
-    return (
-        <View>
-            <View style={styles.iconHolder}></View>
-            <Text style={styles.text}>{props.deviceName} </Text>
-        </View>     
-    );
+  return (
+    <View>
+      <View style={styles.iconHolder}></View>
+      <Text style={styles.text}>{props.deviceName} </Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    iconHolder: {
-        backgroundColor: "green",
-        borderRadius: 41,
-        width: 70,
-        height: 70
-    },
-    text: {
-        fontSize: 10,
-        width: 70,
-        color: "blue",
-        textAlign: "center"
-    }
-  });
-
+  iconHolder: {
+    backgroundColor: 'green',
+    borderRadius: 41,
+    width: 70,
+    height: 70,
+  },
+  text: {
+    fontSize: 10,
+    width: 70,
+    color: 'blue',
+    textAlign: 'center',
+  },
+});
 
 export default GuestElement;
--- screens/Application/GuestsScreen.js
+++ screens/Application/GuestsScreen.js
@@ -1,103 +1,133 @@
 import React, { useState, useEffect } from 'react';
-import { TouchableWithoutFeedback, StyleSheet, Keyboard, TouchableOpacity, View, Text, ScrollView } from 'react-native';
+import {
+  TouchableWithoutFeedback,
+  StyleSheet,
+  Keyboard,
+  TouchableOpacity,
+  View,
+  Text,
+  ScrollView,
+} from 'react-native';
 import { connect } from 'react-redux';
 import SearchBar from '../../components/SearchBar';
 import { getHubInfoAction } from '../../redux/Action/getHubInfoAction';
 import { registerHubAction } from '../../redux/Action/registerHubAction';
 import appStyle from '../../styles/AppStyle';
 import DeviceInfoCard from '../../components/cards/DeviceInfoCard';
+import Modal from 'react-native-modal';
 
 // AWS Config
 const AWS = require('aws-sdk');
 AWS.config.update({ region: 'us-east-1' });
 
-
-
 function GuestsScreen(props) {
+  const [searchParam, setSearchParam] = useState('');
+  const [isVisible, setIsVisible] = useState(false);
 
-	// console.log(props);
+  openModal = () => {
+    setIsVisible(!isVisible);
+  };
+  // console.log(props);
 
-	// =========================================================================
-	// REFRESH LOGIC: TO DO LATER
-	// =========================================================================
+  // =========================================================================
+  // REFRESH LOGIC: TO DO LATER
+  // =========================================================================
 
-			// Retrieves all the information on pull down/refresh of the app
-			// onRefresh = async () => {
-			// 	// await this.setState({ loading: true });
-			// 	// await this.setState({ refreshingUsers: true });
-			// 	const { idToken } = props.sessionData;
-			// 	props.getHub(idToken);
-			// 	props.getDevices(idToken);
-			// 	await props.getAccounts(idToken);
-			// 	props.getSharedDevices(idToken);
-			// 	getUsageLogs();
-			// 	getAccessLogs();
-			// 	// await this.setState({ loading: false });
-			// 	// await this.setState({ refreshingUsers: false });
-			// };
+  // Retrieves all the information on pull down/refresh of the app
+  // onRefresh = async () => {
+  // 	// await this.setState({ loading: true });
+  // 	// await this.setState({ refreshingUsers: true });
+  // 	const { idToken } = props.sessionData;
+  // 	props.getHub(idToken);
+  // 	props.getDevices(idToken);
+  // 	await props.getAccounts(idToken);
+  // 	props.getSharedDevices(idToken);
+  // 	getUsageLogs();
+  // 	getAccessLogs();
+  // 	// await this.setState({ loading: false });
+  // 	// await this.setState({ refreshingUsers: false });
+  // };
 
-	// Called when when the screen is about to load, grabs all the info to display
-	// THE LACK OF THE [] IN THE SECOND PARAMETER CAUSES INFINITE CONSOLE LOGGING OR RERENDERING IN ACCOUNT SCREEN
-	// REQUIRES FURTHER INVESTIGATION
-	useEffect(() => {
-		// if (props.sessionData != null)
-		// 	props.navigation.setParams({
-		// 		name: props.sessionData.name
-		// 	});
-		const { idToken } = props.sessionData;
-		props.getHub(idToken);
-		props.getDevices(idToken);
-		// await props.getAccounts(idToken);
-		props.getSharedDevices(idToken);
-		//getUsageLogs();
-		//getAccessLogs();
-		// onRefresh();
-	}, []);
+  // Called when when the screen is about to load, grabs all the info to display
+  // THE LACK OF THE [] IN THE SECOND PARAMETER CAUSES INFINITE CONSOLE LOGGING OR RERENDERING IN ACCOUNT SCREEN
+  // REQUIRES FURTHER INVESTIGATION
+  useEffect(() => {
+    // if (props.sessionData != null)
+    // 	props.navigation.setParams({
+    // 		name: props.sessionData.name
+    // 	});
+    const { idToken } = props.sessionData;
+    props.getHub(idToken);
+    props.getDevices(idToken);
+    // await props.getAccounts(idToken);
+    props.getSharedDevices(idToken);
+    //getUsageLogs();
+    //getAccessLogs();
+    // onRefresh();
+  }, []);
 
-	const [searchParam, setSearchParam] = useState('');
-	
-    return (
-		<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-			<View  style={appStyle.container}>
-				<View style={styles.header}>
-                	<SearchBar setSearchParam={setSearchParam}/>
-					<View style={styles.button}>
-						<Text>Add Guest</Text>
-					</View>
-				</View>
-                {/* <Text>{searchParam}</Text> */}
-                <ScrollView style={styles.cardContainer}>
-                    <DeviceInfoCard title = {"Sam Smith"} type={"GuestCard"}/>
-                </ScrollView>
-			</View>
-		</TouchableWithoutFeedback>
-    );
+  let modal = (
+    <Modal
+      visible={isVisible}
+      transparent={true}
+      onBackdropPress={() => setIsVisible(false)}
+    >
+      <View style={styles.modal}>
+        <Text>Test</Text>
+      </View>
+    </Modal>
+  );
 
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={appStyle.container}>
+        <View style={styles.header}>
+          <SearchBar setSearchParam={setSearchParam} />
+          <View style={styles.button}>
+            <TouchableOpacity onPress={() => openModal()}>
+              <Text>Add Guest</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        {/* <Text>{searchParam}</Text> */}
+        <ScrollView style={styles.cardContainer}>
+          <DeviceInfoCard title={'Sam Smith'} type={'GuestCard'} />
+        </ScrollView>
+        {modal}
+      </View>
+    </TouchableWithoutFeedback>
+  );
 }
 
 const styles = StyleSheet.create({
-    cardContainer: {
-		flex: 1,
-		width: "100%",
-    },
-	header: {
-		flexDirection: "row",
-		width: "100%",
-		marginTop: 50,
-		justifyContent: "flex-start",
-		alignItems: "center"
-	},
-	button: {
-		position: "absolute",
-		backgroundColor: "blue",
-		height: 70,
-		right: 0,
-		width: "30%",
-		justifyContent: 'center',
-		alignItems: "center",
-		borderRadius: 15
-	}
-  });
-
+  cardContainer: {
+    flex: 1,
+    width: '100%',
+  },
+  header: {
+    flexDirection: 'row',
+    width: '100%',
+    marginTop: 50,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  button: {
+    position: 'absolute',
+    backgroundColor: 'blue',
+    height: 70,
+    right: 0,
+    width: '30%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 15,
+  },
+  modal: {
+    backgroundColor: 'green',
+    width: 300,
+    height: 500,
+    alignSelf: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default GuestsScreen;
