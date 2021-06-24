eb1776ea506be2baeb695464a692f1ed86d7e982
ScottFiletti <59986177+ScottFiletti@users.noreply.github.com>
2021-06-24 19:10:18 -0400

    Add Guest And Device Modal
    Implemented Add Device without propogating functionality. Adjusted Add Guest Modal to also work with the Add Guest Button.
    
    Note: Both Add Guest modals will need Pauls functionality from Tuesday

--- components/cards/ListEntries/SampleDeviceList.js
+++ components/cards/ListEntries/SampleDeviceList.js
@@ -21,6 +21,7 @@ function SampleDeviceList(props) {
   const [screen, setScreen] = useState("");
   const [isVisible, setIsVisible] = useState(false);
   const [isVisible2, setIsVisible2] = useState(false);
+  const [isVisibleDevices, setIsVisibleDevices] = useState(false);
   const [selected, setSelected] = useState(null);
   const [sharedAccs, setSharedAccs] = useState(null);
   const [guestEmail, setGuestEmail] = useState("");
@@ -69,6 +70,10 @@ function SampleDeviceList(props) {
     setIsVisible(!isVisible);
   };
 
+  const openModalDevices = () => {
+    setIsVisibleDevices(!isVisibleDevices);
+  }
+
   const handleClick = () => {
     setIsVisible(false);
     setIsVisible2(true);
@@ -84,7 +89,7 @@ function SampleDeviceList(props) {
   let addButton = (
     <View style={styles.iconAndName}>
       <TouchableOpacity
-        onPress={() => openModal()}
+        onPress={() => screen == "Devices" ? openModal() : openModalDevices() }
         style={screen == "Devices" ? styles.addGuest : styles.addDevice}
       >
         <Icon name="plus" type="font-awesome" color="#FFFFFF" size={38} />
@@ -175,6 +180,72 @@ function SampleDeviceList(props) {
     </Modal>
   );
 
+
+  let modalDevices = (
+    <Modal
+      visible={isVisibleDevices}
+      transparent={true}
+      onBackdropPress={() => setIsVisibleDevices(false)}
+    >
+      <View style={styles.modal}>
+        <View style={styles.topGuestModal}>
+          <Icon name="codesandbox" type="feather" color="black" />
+          <Text style={{ marginLeft: 10, fontSize: 20 }}>Add Device</Text>
+        </View>
+
+        {props.sharedAccountsData.sharedAccounts &&
+          props.sharedAccountsData.sharedAccounts.map((entry, i) => (
+            <View key={i} style={styles.cardCon}>
+              <TouchableOpacity onPress={() => setSelected(entry)}>
+                <View
+                  style={{
+                    paddingLeft: 10,
+                    paddingVertical: 6,
+                    flexDirection: "row",
+                    borderRadius: 10,
+                    elevation: selected == entry ? 2 : 0,
+                    backgroundColor: selected == entry ? "white" : "#F1F1F1",
+                  }}
+                >
+                  <Image source={require("../../../assets/people.png")} />
+                  <Text style={styles.cardText}>{entry.name}</Text>
+                </View>
+              </TouchableOpacity>
+              <View style={styles.seperator}></View>
+            </View>
+          ))}
+
+    
+        <View style={{ flex: 1, marginBottom: 30, justifyContent: "flex-end" }}>
+          <TouchableOpacity onPress={() => propsClick()}>
+            <View
+              style={{
+                marginTop: 35,
+                backgroundColor: "#289EFF",
+                borderRadius: 10,
+                width: 150,
+                height: 40,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Text
+                style={{
+                  textAlign: "center",
+                  fontSize: 22,
+                  fontWeight: "bold",
+                  color: "white",
+                }}
+              >
+                Add Device
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  );
+
   let modal2 = (
     <Modal
       visible={isVisible2}
@@ -235,6 +306,7 @@ function SampleDeviceList(props) {
       {addButton}
       {modal}
       {modal2}
+      {modalDevices}
     </View>
   );
 }
