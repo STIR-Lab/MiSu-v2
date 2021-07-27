4beb3086605b39475f4a31fbbadb8ddff9abb726
Sebastian Fabara <sbs3128@live.com>
2021-07-26 21:31:08 -0400

    Set Schedule ui edits

--- screens/Application/SetScheduleScreen.js
+++ screens/Application/SetScheduleScreen.js
@@ -16,6 +16,7 @@ import xtype from "xtypejs";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import DateTimePicker from "@react-native-community/datetimepicker";
 //const weekdays = ['Monday', 'Tuesday', 'Wednesday', "Thursday", "Friday", "Saturday", "Sunday"];
+import { Icon } from "react-native-elements";
 
 //Import Header Component
 import Header from "../../components/app/Header.js";
@@ -180,6 +181,7 @@ const SetScheduleScreen = (props) => {
       <View style={styles.time}>
         <Text style={styles.timeText}>Week Days</Text>
         <DropDownPicker
+          zIndex={10000}
           placeholder="Select Days"
           style={styles.picker}
           multiple={true}
@@ -191,13 +193,20 @@ const SetScheduleScreen = (props) => {
           setItems={setItems}
           open={open}
           value={value}
+          modalContentContainerStyle={{ zIndex: 100000 }}
+          dropDownContainerStyle={{
+            zIndex: 100000,
+            borderWidth: 0,
+          }}
           containerStyle={{
             width: 170,
             borderColor: "white",
-            elevation: 6
+
+            zIndex: 100000,
           }}
           selectedItemContainerStyle={{
             backgroundColor: "#5F9EE950",
+            zIndex: 100000,
           }}
         />
       </View>
@@ -210,6 +219,8 @@ const SetScheduleScreen = (props) => {
       <View style={styles.time}>
         <Text style={styles.timeText}>All Day</Text>
         <Switch
+          thumbColor={allDay ? "#7DEA7B" : "white"}
+          trackColor={{ false: "#767577", true: "#caedca" }}
           value={allDay}
           onValueChange={(val) => {
             setAllDay(val);
@@ -248,7 +259,15 @@ const SetScheduleScreen = (props) => {
     return (
       <View style={styles.time}>
         <Text style={styles.timeText}>Start Time</Text>
-        <Button title="Start Time" onPress={showTimePicker} />
+        <TouchableOpacity
+          onPress={showTimePicker}
+          title="Start Time"
+          style={styles.timeBtn}
+        >
+          <Text style={styles.timeBtnText}>
+            {startTime == null ? "Set Time!" : startTime}
+          </Text>
+        </TouchableOpacity>
         <DateTimePickerModal
           isVisible={isTimePickerVisible}
           mode="time"
@@ -289,7 +308,17 @@ const SetScheduleScreen = (props) => {
     return (
       <View style={styles.time}>
         <Text style={styles.timeText}>End Time</Text>
-        <Button title="End Time" onPress={showTimePicker} />
+
+        <TouchableOpacity
+          onPress={showTimePicker}
+          title="Start Time"
+          style={styles.timeBtn}
+        >
+          <Text style={styles.timeBtnText}>
+            {endTime == null ? "Set Time!" : endTime}
+          </Text>
+        </TouchableOpacity>
+
         <DateTimePickerModal
           isVisible={isTimePickerVisible}
           mode="time"
@@ -321,7 +350,15 @@ const SetScheduleScreen = (props) => {
     return (
       <View style={styles.time}>
         <Text style={styles.timeText}>Start Date</Text>
-        <Button title="Start Date" onPress={showDatePicker} />
+        <TouchableOpacity style={styles.dateBtn} onPress={showDatePicker}>
+          <Text style={styles.dateBtnText}>
+            {startDate == null ? "Set Start" : startDate}
+          </Text>
+          <View style={styles.dateIcon}>
+            <Icon name="calendar" type="feather" size={31} color="white" />
+          </View>
+        </TouchableOpacity>
+
         <DateTimePickerModal
           isVisible={isDatePickerVisible}
           mode="date"
@@ -351,7 +388,16 @@ const SetScheduleScreen = (props) => {
     return (
       <View style={styles.time}>
         <Text style={styles.timeText}>End Date</Text>
-        <Button title="End Date" onPress={showDatePicker} />
+
+        <TouchableOpacity style={styles.dateBtn} onPress={showDatePicker}>
+          <Text style={styles.dateBtnText}>
+            {endDate == null ? "Set End" : endDate}
+          </Text>
+          <View style={styles.dateIcon}>
+            <Icon name="calendar" type="feather" size={31} color="white" />
+          </View>
+        </TouchableOpacity>
+
         <DateTimePickerModal
           isVisible={isDatePickerVisible}
           mode="date"
@@ -371,14 +417,17 @@ const SetScheduleScreen = (props) => {
       </View>
       <View style={styles.setTime}>
         <WeekDays />
-        <AllDay val={allDay} />
+        <AllDay val={allDay} zIndex={-1000} />
         <StartTime />
         <StartDate />
         <EndTime />
         <EndDate />
       </View>
-      <View style={styles.setRepeat}>
-        <Button onPress={() => handleSave()} title="Save" />
+
+      <View style={styles.saveContainer}>
+        <TouchableOpacity style={styles.saveBtn} onPress={() => handleSave()}>
+          <Text style={styles.saveText}>Save</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -434,8 +483,9 @@ const styles = StyleSheet.create({
     paddingBottom: 15,
   },
   setTime: {
-    flex: 1,
+    flex: 0.78,
     flexDirection: "column",
+    width: "95%",
   },
   time: {
     flexDirection: "row",
@@ -448,7 +498,7 @@ const styles = StyleSheet.create({
     width: "100%",
   },
   timeText: {
-    paddingLeft: 100,
+    paddingLeft: 60,
     fontSize: 14,
     fontWeight: "bold",
   },
@@ -458,6 +508,7 @@ const styles = StyleSheet.create({
     elevation: 6,
     width: 170,
     height: 40,
+    zIndex: 10000,
   },
   setRepeat: {
     backgroundColor: "transparent",
@@ -528,7 +579,73 @@ const styles = StyleSheet.create({
     color: "white",
     fontWeight: "bold",
   },
-  switch: {},
+  timeBtn: {
+    backgroundColor: "white",
+    width: 110,
+    height: 45,
+    elevation: 10,
+    borderRadius: 8,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    zIndex: -10000,
+  },
+  timeBtnText: {
+    fontSize: 22,
+    zIndex: -10000,
+  },
+  dateBtn: {
+    backgroundColor: "white",
+    width: 160,
+    height: 45,
+    elevation: 10,
+    borderRadius: 8,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+  dateIcon: {
+    backgroundColor: "#44ABFF",
+    width: "25%",
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    zIndex: 1000,
+    borderBottomRightRadius: 8,
+    borderTopRightRadius: 8,
+  },
+  dateBtnText: {
+    width: "75%",
+    height: "60%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    fontSize: 20,
+  },
+  saveBtn: {
+    width: "25%",
+    height: 50,
+    backgroundColor: "#44ABFF",
+    borderRadius: 10,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    elevation: 10,
+  },
+  saveText: {
+    fontSize: 20,
+    color: "white",
+  },
+
+  saveContainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 20,
+  },
 });
 
 export default SetScheduleScreen;
