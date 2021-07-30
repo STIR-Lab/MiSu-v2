0ed1a3d3703880c313eb34a707bcc9932774d0a1
Paul <colandreapaul@knights.ucf.edu>
2021-07-29 20:14:03 -0400

    Fix for refresh on removing guest

--- redux/Action/stopSharing.js
+++ redux/Action/stopSharing.js
@@ -1,32 +1,24 @@
-import { deleteASharedAccount } from '../../services/deleteService'
-import { getSharedAccountsAction } from '../Action/getSharedAccountsAction'
-import { getSharedDevicesAction } from '../Action/getSharedDevicesAction'
+import { deleteASharedAccount } from "../../services/deleteService";
+import { getSharedAccountsAction } from "../Action/getSharedAccountsAction";
+import { getSharedDevicesAction } from "../Action/getSharedDevicesAction";
 
-const  stopSharingStart =(payload) =>({
-    type: 'STOP_SHARING' ,
-    payload
-})
+const stopSharingStart = (payload) => ({
+  type: "STOP_SHARING",
+  payload,
+});
 
+export const stopSharingAction = (login_id, idToken) => {
+  return async (dispatch) => {
+    try {
+      dispatch(stopSharingStart({ loading: true }));
 
+      await deleteASharedAccount(login_id, idToken);
 
-
-export const stopSharingAction = ( login_id ,devices ,idToken) => { 
-    return async (dispatch) => {
-
-        try {
-            dispatch(stopSharingStart({ loading: true}))
-          
-            const dt = await deleteASharedAccount(login_id, idToken)
-           
-
-            dispatch(stopSharingStart({loading: false}))
-            dispatch(getSharedAccountsAction(idToken))
-            dispatch(getSharedDevicesAction(idToken))
-           
-            
-        } catch (error) {
-            dispatch(stopSharingStart({loading: false}))
-        }
-
+      dispatch(stopSharingStart({ loading: false }));
+      dispatch(getSharedAccountsAction(idToken));
+      dispatch(getSharedDevicesAction(idToken));
+    } catch (error) {
+      dispatch(stopSharingStart({ loading: false }));
     }
-}
+  };
+};
